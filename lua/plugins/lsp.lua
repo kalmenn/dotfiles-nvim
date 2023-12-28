d93299583edf6de0c55b5d97929e483f035add22
kalmenn <kale@kalmenn.fr>
2023-12-28 14:03:59 +0100

    lsp + crates.nvim: fix keybinds

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,38 +1,39 @@
-vim.api.nvim_create_autocmd('LspAttach', {
-    group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-    callback = function(ev)
-        -- Enable completion triggered by <c-x><c-o>
-        vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
-
-        -- Buffer local mappings.
-        -- See `:help vim.lsp.*` for documentation on any of the below functions
-        local opts = { buffer = ev.buf }
-        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-        vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-        vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-        vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
-        vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
-        vim.keymap.set('n', '<space>wl', function()
-            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-        end, opts)
-        vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-        vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-        vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
-        vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-        vim.keymap.set('n', '<space>f', function()
-            vim.lsp.buf.format { async = true }
-        end, opts)
-    end,
-})
-
 return {
     {
         "neovim/nvim-lspconfig",
         dependencies = {
             "hrsh7th/cmp-nvim-lsp",
         },
+        init = function()
+            vim.api.nvim_create_autocmd('LspAttach', {
+                group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+                callback = function(ev)
+                    -- Enable completion triggered by <c-x><c-o>
+                    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+                    -- Buffer local mappings.
+                    -- See `:help vim.lsp.*` for documentation on any of the below functions
+                    local opts = { buffer = ev.buf }
+                    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+                    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+                    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+                    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+                    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+                    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+                    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+                    vim.keymap.set('n', '<space>wl', function()
+                        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+                    end, opts)
+                    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+                    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+                    vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+                    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+                    vim.keymap.set('n', '<space>f', function()
+                        vim.lsp.buf.format { async = true }
+                    end, opts)
+                end,
+            })
+        end,
         config = function()
             local lspconfig = require("lspconfig")
 
@@ -74,6 +75,40 @@ return {
             {
                 "Saecki/crates.nvim",
                 tag = 'stable',
+                init = function()
+                    local crates = require('crates')
+                    vim.api.nvim_create_autocmd("BufEnter", {
+                        pattern = "Cargo.toml",
+                        group = vim.api.nvim_create_augroup('UserCratesNvimConfig', {}),
+                        callback = function(ev)
+                            local opts = { silent = true, buffer = ev.buffer }
+
+                            vim.keymap.set('n', '<leader>ct', crates.toggle, opts)
+                            vim.keymap.set('n', '<leader>cr', crates.reload, opts)
+
+                            vim.keymap.set('n', '<leader>cv', crates.show_versions_popup, opts)
+                            vim.keymap.set('n', '<leader>cf', crates.show_features_popup, opts)
+                            vim.keymap.set('n', '<leader>cd', crates.show_dependencies_popup, opts)
+
+                            vim.keymap.set('n', '<leader>cu', crates.update_crate, opts)
+                            vim.keymap.set('v', '<leader>cu', crates.update_crates, opts)
+                            vim.keymap.set('n', '<leader>ca', crates.update_all_crates, opts)
+                            vim.keymap.set('n', '<leader>cU', crates.upgrade_crate, opts)
+                            vim.keymap.set('v', '<leader>cU', crates.upgrade_crates, opts)
+                            vim.keymap.set('n', '<leader>cA', crates.upgrade_all_crates, opts)
+
+                            vim.keymap.set('n', '<leader>ce', crates.expand_plain_crate_to_inline_table, opts)
+                            vim.keymap.set('n', '<leader>cE', crates.extract_crate_into_table, opts)
+
+                            vim.keymap.set('n', '<leader>cH', crates.open_homepage, opts)
+                            vim.keymap.set('n', '<leader>cR', crates.open_repository, opts)
+                            vim.keymap.set('n', '<leader>cD', crates.open_documentation, opts)
+                            vim.keymap.set('n', '<leader>cC', crates.open_crates_io, opts)
+                        end,
+                    })
+                end,
+                opts = {},
+                event = "BufEnter Cargo.toml",
             },
         },
         opts = {
