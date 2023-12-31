b48a48371a41fabb9ef7ef5d10737ef663a9984b
kalmenn <kale@kalmenn.fr>
2023-12-31 11:43:21 +0100

    feline: removed unused settings

--- lua/plugins/feline.lua
+++ lua/plugins/feline.lua
@@ -4,11 +4,5 @@ return {
         "lewis6991/gitsigns.nvim",
     },
     lazy = false,
-    config = function(_, opts)
-        require('feline').setup()
-        -- require('feline').winbar.setup()       -- to use winbar
-        require('feline').statuscolumn.setup() -- to use statuscolumn
-
-        -- require('feline').use_theme()          -- to use a custom theme
-    end
+    opts = {},
 }
