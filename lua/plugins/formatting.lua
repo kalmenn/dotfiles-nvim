01c084a5a39d5a1feea73d92a599ac7481281ad8
kalmenn <kale@kalmenn.fr>
2024-01-07 18:13:27 +0100

    formatting: set up null-ls and mason-null-ls

---
+++ lua/plugins/formatting.lua
@@ -0,0 +1,27 @@
+return {
+    {
+        "nvimtools/none-ls.nvim",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+        config = function()
+            local null_ls = require("null-ls")
+
+            null_ls.setup({
+                sources = {
+                },
+            })
+        end,
+    },
+    {
+        "jay-babu/mason-null-ls.nvim",
+        dependencies = {
+            "williamboman/mason.nvim",
+            "nvimtools/none-ls.nvim",
+        },
+        opts = {
+            ensure_installed = { "stylua" },
+            handlers = {},
+        },
+    },
+}
