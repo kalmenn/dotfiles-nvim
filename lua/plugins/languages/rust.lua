fd21ca18eb501ed2e075cec4c1d788255a04694f
kalmenn <kale@kalmenn.fr>
2024-02-29 23:13:05 +0100

    feat(lsp)!: Remove rust-tools hover and code actions keybindings

--- lua/plugins/languages/rust.lua
+++ lua/plugins/languages/rust.lua
@@ -49,13 +49,6 @@ return {
 
             require("rust-tools").setup({
                 server = {
-                    on_attach = function(_, bufnr)
-                        local rt = require("rust-tools")
-                        -- Hover actions
-                        vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
-                        -- Code action groups
-                        vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
-                    end,
                     capabilities = capabilities,
                     settings = {
                         ["rust-analyzer"] = {
